318bcd46c729caf3994a80161f260647235e37d7
Will Turner <turnejam@gmail.com>
2017-11-20 10:37:53 -0500

    Assignment complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,16 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+  
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entries.delete_at(index)
+        return
+      else
+        index +=1
+      end
+    end
+  end
 end
