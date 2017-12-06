0265eec1354220786d36d3d782e3f9dafdda5773
Will Turner <turnejam@gmail.com>
2017-12-06 11:27:46 -0500

    Checkpoint 7

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,7 +20,7 @@ class AddressBook
   def remove_entry(name, phone_number, email)
     entries.each do |entry|
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        entries.remove(entry)
+        entries.delete(entry)
       end
     end
   end
@@ -33,4 +33,24 @@ class AddressBook
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
+  
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+    
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid-1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+      
+    return nil
+  end
+  
 end
